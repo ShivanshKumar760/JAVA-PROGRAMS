56d8a481bdb1514d6e754f3037bf6afc9dffae80
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2022-12-27 11:37:17 +0530

    Add files via upload

---
+++ ExampleToDeclareVariableType3/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int num= 2147483647;//integer max value
+        long num1= 5555555555555555555L;
+        long num2= 9223372036854775807L;//long max value
+        short num3 = 32767;//short max value
+        char c='a';
+        boolean bool=true;
+        boolean bool2=false;
+        String name="David";
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(c);
+        System.out.println(bool);
+        System.out.println(bool2);
+        System.out.println(name);
+
+    }
+}
