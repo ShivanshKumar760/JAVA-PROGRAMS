a1ed202d0ad5744ddb7ca8cd17c97ca3c44a4d0b
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2022-12-27 11:20:54 +0530

    Add files via upload

---
+++ DataTypeAndVariable2/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+    /*
+    types of datatype in java are:
+
+    int --- integer(used to represent whole number ie without decimal point  32 bits ie 4byte
+    short - is a 16 bit ie 2 byte integer data type to store small values
+    long --2s compliment integer it works like int but can store large values
+    float -- used to represent number with decimal point
+    double -- while float has preciseness till 7 decimal double offers till 16 decimal
+    bool-- boolean data type ie binary value of true and false
+    char -- character data type is used to represent character ie single letter
+    string -- string is used to represent collection of letter to form word or sentence */
+    /*
+    Variable declaration syntax:
+
+    data_type variable_name;
+     */
+     /*
+     EXAMPLES
+     */
+
+        int number;
+        char character;
+        float decimal_number;
+        String name ;
+        
+    }
+}
